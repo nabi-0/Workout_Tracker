1a3314a7eeac3d11619ea7ce5790d7720525c5b9
nabi-0 <62216642+nabi-0@users.noreply.github.com>
2020-08-14 17:59:26 -0500

    routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,9 +3,10 @@ const Transaction = require("../models/Workout.js");
 const Workout = require("../models/Workout.js");
 
 router.put("/api/workouts/:id", ({ body }, res) => {
-    Transaction.create(body)
-        .then(dbTransaction => {
-            res.json(dbTransaction);
+    Workout.create(body)
+        .then(({ _id }) => Workout.findByIdAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
+        .then(dbWorkout => {
+            res.json(dbWorkout);
         })
         .catch(err => {
             res.status(400).json(err);
