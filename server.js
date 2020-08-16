6670b2c9ba09c7114d730aef69dcc8b39f67ed2c
nabi-0 <62216642+nabi-0@users.noreply.github.com>
2020-08-15 21:52:12 -0500

    MongoDB URL

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
